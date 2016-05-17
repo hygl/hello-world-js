e0cb881f238bc98a189a48b91c06eefa49e24bc2
Philipp Hügelmeyer <p.huegelmeyer@gmail.com>
2016-05-17 06:08:33 +0000

    Modified tests for demo.

--- tests/unit_tests.js
+++ tests/unit_tests.js
@@ -14,7 +14,7 @@ describe('loading express', function () {
   });
   it('404 everything else', function testPath(done) {
     request(server)
-      .get('/foo/bar')
+      .get('/user/user3')
       .expect(404, done);
   });
    it('return correct user', function testPath(done) {
