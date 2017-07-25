fbfad40cf09417a92c4e6c91c0e73190c37f7710
HUEGELPH <philipp.huegelmeyer@ble.de>
2017-07-25 09:20:57 +0200

    fixes for openshift

--- server.js
+++ server.js
@@ -7,20 +7,20 @@ var users = require('./users');
 
 // App
 const app = express();
-app.get('/', function (req, res) {
-  res.status(200);
-  res.send('Hello world\n');
+app.get('/', function(req, res) {
+    res.status(200);
+    res.send('Hello world\n');
 });
 
 app.get('/user/:user', function(req, res) {
     if (users.list.indexOf(req.params.user) === -1) {
-      return res.status(status.NOT_FOUND).
+        return res.status(status.NOT_FOUND).
         json({ error: 'Not Found' });
     }
     res.json({ user: req.params.user });
-  });
-
-
-var server = app.listen(process.env.PORT, process.env.IP);
+});
+var ip = process.env.IP || '0.0.0.0';
+var port = process.env.PORT || 3000;
+var server = app.listen(port, ip);
 console.log('Running on https://' + process.env.IP + ':' + process.env.PORT);
 module.exports = server
